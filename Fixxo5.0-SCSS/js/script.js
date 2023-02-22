25500cf40b2bff223bada124931336bef0f34441
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2023-02-22 10:51:18 +0100

    showcase done

--- Fixxo5.0-SCSS/js/script.js
+++ Fixxo5.0-SCSS/js/script.js
@@ -1,4 +1,15 @@
 fetch('https://kyh-net22.azurewebsites.net/api/products/%7Btag%7D')
-.then(Response =>Response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error))
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+    $.ajax({
+        url: 'https://index.html',
+        method: 'GET',
+        success: function(products) {
+          // Iterate over each product and append it to the page
+            $(products).each(function() {
+              $('#featured-products').append(this.products);
+        });
+      }
+      });
