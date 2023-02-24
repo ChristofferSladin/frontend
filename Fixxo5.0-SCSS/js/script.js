b720b67b7b36c5616d2ba50d0a735895ccc9c4aa
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2023-02-24 16:30:42 +0100

    trying to style mobile first, doing rest of breakpoints later

--- Fixxo5.0-SCSS/js/script.js
+++ Fixxo5.0-SCSS/js/script.js
@@ -27,7 +27,7 @@
 //     });
 //   }
 
-function get8Products() {
+function getFeaturedProducts() {
     fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
       .then(response => response.json())
       .then(data => {
@@ -38,11 +38,19 @@ function get8Products() {
           productEl.setAttribute('id', `product-${i}`);
           productEl.classList.add('product-card');
           productEl.innerHTML = `
-            <h2 class="product-card-title">${product.name}</h2>
+          <div class="product-card-image-container">
             <img src="${product.image_link}" alt="${product.name}"/>
-             
-            <p class="product-card-price">Price: ${product.price}</p>
-            <a href="#" class="btn-theme">QUICK VIEW</a>`;
+            <div class="product-card-menu">
+                    <nav class="menu-icons">
+                        <a class="menu-link" href="#"><i class="fa-regular fa-code-compare"></i></a>
+                        <a class="menu-link" href="#"><i class="fa-regular fa-heart"></i></a>
+                        <a class="menu-link" href="#"><i class="fa-regular fa-bag-shopping"></i></a>                   
+                    </nav>
+                    <a href="#" class="btn-theme">QUICK VIEW</a>
+                </div>
+          </div>
+            <h2 class="product-card-title">${product.name}</h2>
+            <p class="product-card-price">Price: ${product.price}</p>`;
           productsEl.appendChild(productEl);
         }
       })
